2c8a14a0039cab57b517162e10958a7af22621c8
Madarász Anna <madarasz.anna.9@gmail.com>
2020-03-19 17:31:34 +0100

    1.0

---
+++ Quiz_2020/Form_Nyito.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Quiz_2020
+{
+    partial class Form_Nyito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kategóriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.belépésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.súgóToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kategóriaToolStripMenuItem,
+            this.belépésToolStripMenuItem,
+            this.súgóToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kategóriaToolStripMenuItem
+            // 
+            this.kategóriaToolStripMenuItem.Name = "kategóriaToolStripMenuItem";
+            this.kategóriaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.kategóriaToolStripMenuItem.Text = "Kategória";
+            // 
+            // belépésToolStripMenuItem
+            // 
+            this.belépésToolStripMenuItem.Name = "belépésToolStripMenuItem";
+            this.belépésToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.belépésToolStripMenuItem.Text = "Belépés";
+            // 
+            // súgóToolStripMenuItem
+            // 
+            this.súgóToolStripMenuItem.Name = "súgóToolStripMenuItem";
+            this.súgóToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.súgóToolStripMenuItem.Text = "Súgó";
+            // 
+            // Form_Nyito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form_Nyito";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kategóriaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem belépésToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem súgóToolStripMenuItem;
+    }
+}
+
---
+++ Quiz_2020/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Quiz_2020
+{
+    static class Program
+    {
+        /// <summary>
+        /// Madarász Anna
+        /// 
+        /// 
+        /// 
+        /// </summary>
+       
+        static MySqlConnection conn = null;
+        static MySqlCommand sql = null;
+
+        static void Main()
+        {
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Server = "localhost";
+            sb.UserID = "root";
+            sb.Password = "";
+            sb.Database = "quizz";
+            sb.CharacterSet = "UTF8";
+            conn = new MySqlConnection(sb.ToString());
+            try
+            {
+                conn.Open(); //-- K
+                sql = conn.CreateCommand();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Environment.Exit(0);
+            }
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form_Nyito());
+        }
+    }
+}
